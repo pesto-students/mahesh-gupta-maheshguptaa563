33b26cd9f8f0d4717f3e47b2a4abe24b1673ada3
maheshguptaa563 <maheshguptaa563@gmail.com>
2022-06-24 23:24:20 +0530

    success

---
+++ Week-8/aasgn2.js
@@ -0,0 +1,31 @@
+class Node {
+  constructor(item) {
+    this.data = item;
+    this.left = this.right = null;
+  }
+}
+let root;
+
+function isBST(node) {
+  if (node === null) {
+    return true;
+  }
+  if (node.left != null && node.left.data > node.data) {
+    return false;
+  }
+  if (node.right != null && node.right.data < node.data) {
+    return false;
+  }
+  if (!isBST(node.left) || !isBST * node.right) {
+    return false;
+  }
+  return true;
+}
+
+root = new Node(9);
+root.left = new Node(8);
+root.right = new Node(12);
+root.left.left = new Node(4);
+root.left.right = new Node(10);
+
+console.log(isBST(root));
